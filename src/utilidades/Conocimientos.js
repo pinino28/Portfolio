a5023b837a77e7600dfd177f76337b9ac1307edc
= <=>
2022-11-13 04:38:24 -0300

    visual fixes

--- src/utilidades/Conocimientos.js
+++ src/utilidades/Conocimientos.js
@@ -11,26 +11,22 @@ class SobreMi extends Component{
       $("#typescriptText").hide()
       $("#javascriptText").hide()
       $("#typescript").on("mouseover", function(){
-        $("#typescript").hide()
-        $("#javascript").hide()
+       
         $("#typescriptText").show()
         
       })
       $("#typescript").on("mouseout", function(){
-       $("#typescript").show()
-       $("#javascript").show()
+       
        $("#typescriptText").hide()
       });
 
       //JavaScript texto muestra
       $("#javascript").on("mouseover", function(){
-        $("#typescript").hide()
-        $("#javascript").hide()
+       
         $("#javascriptText").show()
        });
        $("#javascript").on("mouseout", function(){
-        $("#typescript").show()
-        $("#javascript").show()
+        
         $("#javascriptText").hide()
        });
 
@@ -44,69 +40,45 @@ class SobreMi extends Component{
 
         //para D3
         $("#d3").on("mouseover", function(){
-          $("#d3").hide()
-          $("#react").hide()
-          $("#css").hide()
-          $("#jQuery").hide()
+          
           $("#d3Text").show()
         })
 
         $("#d3").on("mouseout", function(){
-          $("#d3").show()
-          $("#react").show()
-          $("#css").show()
-          $("#jQuery").show()
+         
           $("#d3Text").hide()
         })
 
         //para React
         $("#react").on("mouseover", function(){
-          $("#d3").hide()
-          $("#react").hide()
-          $("#css").hide()
-          $("#jQuery").hide()
+         
           $("#reactText").show()
         })
 
         $("#react").on("mouseout", function(){
-          $("#d3").show()
-          $("#react").show()
-          $("#css").show()
-          $("#jQuery").show()
+         
           $("#reactText").hide()
         })
 
         //para CSS
         $("#css").on("mouseover", function(){
-          $("#d3").hide()
-          $("#react").hide()
-          $("#css").hide()
-          $("#jQuery").hide()
+         
           $("#cssText").show()
         })
 
         $("#css").on("mouseout", function(){
-          $("#d3").show()
-          $("#react").show()
-          $("#css").show()
-          $("#jQuery").show()
+         
           $("#cssText").hide()
         })
 
         //para jQuery
         $("#jQuery").on("mouseover", function(){
-          $("#d3").hide()
-          $("#react").hide()
-          $("#css").hide()
-          $("#jQuery").hide()
+          
           $("#jQueryText").show()
         })
 
         $("#jQuery").on("mouseout", function(){
-          $("#d3").show()
-          $("#react").show()
-          $("#css").show()
-          $("#jQuery").show()
+         
           $("#jQueryText").hide()
         })
       })
@@ -121,46 +93,34 @@ class SobreMi extends Component{
      
      //para github
       $("#github").on("mouseover", function(){
-        $("#github").hide()
-        $("#git").hide()
-        $("#VScode").hide()
+       
         $("#githubText").show()
       })
 
       $("#github").on("mouseout", function(){
-        $("#github").show()
-        $("#git").show()
-        $("#VScode").show()
+      
         $("#githubText").hide()
       })
 
      //para git
       $("#git").on("mouseover", function(){
-        $("#github").hide()
-        $("#git").hide()
-        $("#VScode").hide()
+        
         $("#gitText").show()
       })
   
       $("#git").on("mouseout", function(){
-        $("#github").show()
-        $("#git").show()
-        $("#VScode").show()
+       
         $("#gitText").hide()
       })
 
      //para VScode
       $("#VScode").on("mouseover", function(){
-        $("#github").hide()
-        $("#git").hide()
-        $("#VScode").hide()
+       
         $("#VScodeText").show()
       })
      
       $("#VScode").on("mouseout", function(){
-        $("#github").show()
-        $("#git").show()
-        $("#VScode").show()
+     
         $("#VScodeText").hide()
       })
     });
@@ -169,12 +129,12 @@ class SobreMi extends Component{
     $(function(){
       $("#mysqlText").hide()
       $("#mysql").on("mouseover", function(){
-       $("#mysql").hide()
+     
        $("#mysqlText").show()
       })
        
       $("#mysql").on("mouseout", function(){
-       $("#mysql").show()
+       
        $("#mysqlText").hide()
       })
     })
@@ -203,7 +163,7 @@ class SobreMi extends Component{
             
              <img id="javascript" src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Unofficial_JavaScript_logo.svg" alt="JavaScript" className="imgLenguaje"/>
              
-             <p id="javascriptText" style={mouseOnEstilo}><strong>JavaScript</strong></p>
+             
             </div>
             </div>
           </div>
@@ -218,13 +178,7 @@ class SobreMi extends Component{
                   <img id="css" src="https://cdn.iconscout.com/icon/free/png-256/css-logo-3002030-2496146.png" alt="CSS" className="imgFront"/>
                   <img id="jQuery" src="http://cdn.onlinewebfonts.com/svg/img_193692.png" alt="jQuery" className="imgFront"/>
        
-                  <p id="d3Text" style={mouseOnEstilo}><strong>D3<br/>(dataVisualization)</strong></p>
-        
-                  <p id="reactText" style={mouseOnEstilo}><strong>React</strong></p>
-        
-                  <p id="cssText" style={mouseOnEstilo}><strong>Css3</strong></p>
-        
-                  <p id="jQueryText" style={mouseOnEstilo}><strong>jQuery</strong></p>
+                  
               </div>
           </div>
 
@@ -251,9 +205,7 @@ class SobreMi extends Component{
                 <img id="github" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github" className="imgUtilidades"/>
                 <img id="git" src="https://git-scm.com/images/logos/downloads/Git-Icon-Black.png" alt="Git" className="imgUtilidades"/>
                 <img id="VScode" src="https://cdn.iconscout.com/icon/free/png-256/vs-code-2336946-1982827.png" alt="VScode" className="imgUtilidades"/>
-                <p id="githubText" style={mouseOnEstilo}><strong>GitHub</strong></p>
-                <p id="gitText" style={mouseOnEstilo}><strong>Git</strong></p>
-                <p id="VScodeText" style={mouseOnEstilo}><strong>VScode</strong></p>
+                
             </div>
           </div>
 
@@ -264,13 +216,24 @@ class SobreMi extends Component{
             DataBases
             </strong>
            </h4>
-           <p id="mysqlText" style={mouseOnEstilo}><strong>MySql</strong></p>
+           
           <img id="mysql" src="https://pngimg.com/uploads/mysql/mysql_PNG3.png" alt="Git" className="imgUtilidades"/>
           </div>
           </div>
 
           <p id="typescriptText" style={mouseOnEstilo}><strong>TypeScript</strong></p>
+          <p id="d3Text" style={mouseOnEstilo}><strong>D3<br/>(dataVisualization)</strong></p>
+        
+        <p id="reactText" style={mouseOnEstilo}><strong>React</strong></p>
 
+        <p id="cssText" style={mouseOnEstilo}><strong>Css3</strong></p>
+
+        <p id="jQueryText" style={mouseOnEstilo}><strong>jQuery</strong></p>
+        <p id="githubText" style={mouseOnEstilo}><strong>GitHub</strong></p>
+                <p id="gitText" style={mouseOnEstilo}><strong>Git</strong></p>
+                <p id="VScodeText" style={mouseOnEstilo}><strong>VScode</strong></p>
+                <p id="mysqlText" style={mouseOnEstilo}><strong>MySql</strong></p>
+                <p id="javascriptText" style={mouseOnEstilo}><strong>JavaScript</strong></p>
         </div>
         
   </div>
